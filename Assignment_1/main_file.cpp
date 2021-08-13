f7c3afd4faa2bca8661e4b1be925317d52fcb02d
mellophi <ayon01051998@gmail.com>
2021-08-13 17:19:15 +0530

    Pushing the main_file and utils header

--- Assignment_1/main_file.cpp
+++ Assignment_1/main_file.cpp
@@ -1,35 +1,100 @@
 #include "stdafx.h"
-#include <string.h>
 #include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<string.h>
+#include <math.h>
+//self created header file to save the utility functions
 #include "utils.h"
 
-const int noise_offset = 45;
-const double frame_size = 100.0, multiplier = 7.0;
+#define noiseFrame 5
+
+int samples[1000000], choice;
+
+double thresholdEnergy = 0, thresholdZCR = 0;
+
 
 int main(){
-	FILE *input, *temp, *features;
-	input = fopen("D:\\IITG\\Semester 1\\Speech Processing\\Input\\yes_4.txt", "r");
-	temp = fopen("temp.txt", "w");
-	features = fopen("features.txt", "a");
+	FILE *input, *output, *input_n, *noise_op;
+	fopen_s(&input, "D:\\IITG\\Semester 1\\Speech Processing\\Input\\yes_sample_4.txt", "r"); //getting the input
+	fopen_s(&input_n, "normalized_input.txt", "a+"); //normalized output is saved in this file
+	fopen_s(&output, "features.txt", "a+"); //extracting the features and saving
+	fopen_s(&noise_op, "noise_features.txt", "a+");//values after removing noise is save in this file
 
-	//removing noise and svaing in a temporary file
-	remove_noise(input, temp, noise_offset, multiplier);
-	fclose(temp);
+	printf("Enter 1: Use process 1 \nEnter 2 : Use process 2\nEnter choice : ");
+	scanf("%d", &choice);
+	printf("-----------------------------------\n");
+	
+	//In this process we are removing the noise and applying necessary pad
+	//then extracting the features and analysing it
+	if(choice == 1){
+		normalize_input(input, input_n);
+		rewind(input_n);
+		remove_noise(input_n, noise_op, 45, 7.2);
+		rewind(noise_op);
+		feature_extractor(noise_op, output);
+		rewind(output);
+		printf("-----------------------------------\n");
+		printf("OUTPUT : %s\n", analyse_features(output));
+	}
+
+	//In this process we are trying to find the start and end marker for a word and then 
+	//analysing the features of that word
+	else{
+		normalize_input(input, input_n);
+		rewind(input_n);
+		feature_extractor(input_n, output);
+
+		char singleLine[100], *stop;
+	
+		//finding the number of segments and the start and stop positions of each of the segments
+		rewind(output);
+		double energy[10000], zcr[10000];
+		long start, end, seg_cnt = 0, frameCount = 0, flag = 0;
+		while(!feof(output)){
+			fgets(singleLine, 100, output);
+			energy[frameCount] = strtod(singleLine, &stop);
+			zcr[frameCount] = atof(stop + 1);
+			frameCount++;
+		}
+
+		//finding threshold noise energy and threshold noise zcr using first 15 valid frames
+		for(int i=0; i<noiseFrame; ++i){
+			thresholdEnergy += energy[i];
+			thresholdZCR += zcr[i];
+		}
+		thresholdEnergy /= noiseFrame;
+		thresholdZCR /= noiseFrame;
+
+		//for each frame input do this to find the start and end points
+		for(int i=0; i<frameCount-3; ++i){
+			if(!flag && energy[i+1] > thresholdEnergy * 3 && energy[i+2] > thresholdEnergy * 3 && energy[i+3] > thresholdEnergy * 3){
+					start = i;
+					flag = 1;
+			}
+			else if(flag && energy[i+1] <= thresholdEnergy * 3 && energy[i+2] <= thresholdEnergy * 3 && energy[i+3] <= thresholdEnergy * 3){
+				end = i;
+				flag = 0;
+				break;
+			}
+		}
+
+
+		printf("START FARME : %d END FRAME : %d\n", start, end);
+		printf("ENERGY OF START FRAME : %lf\nENERGY OF END FRAME : %lf\n", energy[start], energy[end]);
+		printf("-----------------------------------\n");
+		printf("OUTPUT : %s\n", analyse_features(energy, zcr, start, end));
+
+	}
+
+	//cleaning the temporary files created
 	fclose(input);
-
-	temp = fopen("temp.txt", "r");
-	//reading from the temporary file and calculating the energy and zcr and saving it in a different features file
-	feature_extractor(temp, features);
-	fclose(temp);
-	fclose(features);
-
-	features = fopen("features.txt", "r");
-	printf("%s\n", analyse_features(features));
-	fclose(features);
-
+	fclose(output);
+	fclose(noise_op);
+	fclose(input_n);
+	remove("normalized_input.txt");
 	remove("features.txt");
-	remove("temp.txt");
+	remove("noise_features.txt");
+	
 	return 0;
 }
--- Assignment_1/utils.h
+++ Assignment_1/utils.h
@@ -1,3 +1,4 @@
+//function to remove noise from the segmented input
 void remove_noise(FILE *input, FILE *output, int noise_offset, int multiplier){
 	char singleLine[100];
 	while(!feof(input)){
@@ -10,6 +11,7 @@ void remove_noise(FILE *input, FILE *output, int noise_offset, int multiplier){
 	}
 }
 
+//fuction to extract the features of the speech (Energy and ZCR) and save it in a different file
 void feature_extractor(FILE *input, FILE *output){
 	char singleLine[100];
 	double energy = 0, zcr = 0, p_x = 0, cnt = 0;
@@ -17,20 +19,23 @@ void feature_extractor(FILE *input, FILE *output){
 		fgets(singleLine, 100, input);
 		if(!isalpha(singleLine[0])){
 			double x = atof(singleLine);
-			if(cnt == 100){
-				fprintf(output, "%lf %lf\n", (energy/100.0), zcr);
-				energy = 0;
-				cnt = 0;
-				zcr = 0;
+			if(abs(x) > 1){
+				if(cnt == 100){
+					fprintf(output, "%lf %lf\n", (energy/100.0), zcr);
+					energy = 0;
+					cnt = 0;
+					zcr = 0;
+				}
+				energy += x*x;
+				zcr += p_x*x < 0;
+				cnt++;
+				p_x = x;
 			}
-			energy += x*x;
-			zcr += p_x*x < 0;
-			cnt++;
-			p_x = x;
 		}
 	}
 }
 
+//function to analyse the features of the speech and give output accordingly for the entire file
 char* analyse_features(FILE *input){
 	char singleLine[100], *stopstring;
 	double energy, zcr, count_frames = 0, count_fricatives = 0;
@@ -46,4 +51,107 @@ char* analyse_features(FILE *input){
 		return "YES";
 	else
 		return "NO";
+}
+
+//for a perticular part of the file
+char* analyse_features(FILE *input, long start, long end){
+	char singleLine[100], *stopstring;
+	double energy, zcr, count_frames = 0, count_fricatives = 0;
+	fseek(input, start, SEEK_SET);
+	while(!feof(input) && ftell(input) <= end){
+		fgets(singleLine, 100, input);
+		energy = strtod(singleLine, &stopstring);
+		zcr = atof(stopstring+1);
+		if(zcr > 15)
+			count_fricatives ++;
+		count_frames ++;
+	}
+	if(count_fricatives >= count_frames * 0.4)
+		return "YES";
+	else
+		return "NO";
+}
+
+//analyse features using the energy and zcr array
+char* analyse_features(double *energy, double *zcr, long start, long end){
+	double count_fricatives = 0;
+	for(int i=start; i<=end-1; ++i){
+		if(zcr[i] > 15 && zcr[i+1] > 15)
+			count_fricatives ++;
+	}
+	double count_frames = end - start + 1;
+	printf("count frames : %lf\ncount fricatives : %lf\n", count_frames, count_fricatives);
+	if(count_fricatives >= count_frames * 0.3)
+		return "YES";
+	else
+		return "NO";
+}
+
+//creates a temporary file by extracting information from the input file in the given range
+void save_temp(FILE *input, FILE *output, long start, long end){
+	char singleLine[100], *stopstring;
+	fseek(input, start, SEEK_SET);
+	while(!feof(input) && ftell(input) <= end){
+		fgets(singleLine, 100, input);
+		fprintf(output, singleLine);
+	}
+}
+
+//returns the number of null samples in the input (0, 1, -1)
+long count_null(FILE *input){
+	char singleLine[100];
+	long cnt = 0;
+	while(!feof(input)){
+		fgets(singleLine, 100, input);
+		if(!isalpha(singleLine[0])){
+			int x = atoi(singleLine);
+			if(x != 0 && x != 1 && x != -1){
+				cnt = ftell(input);
+				break;
+			}
+		}
+	}
+	return cnt;
+}
+
+//normalizing the input and giving an output file which contains the normalized output
+void normalize_input(FILE *input, FILE *output, int limit = 5000){
+	long max = 0, dcShift = 0, sampleCount = 0;
+	char singleLine[100];
+
+	//calculate DC shift
+	while(!feof(input)){
+		fgets(singleLine, 100, input);
+		if(!isalpha(singleLine[0])){
+			int x = atoi(singleLine);
+			dcShift += x;
+			sampleCount ++;
+		}
+	}
+	dcShift /= sampleCount;
+
+	//calculate normalization factor
+	rewind(input);
+	while(!feof(input)){
+		fgets(singleLine, 100, input);
+		if(!isalpha(singleLine[0])){
+			int x = atoi(singleLine);
+			if(max < abs(x)){
+				max = abs(x);
+			}
+		}
+	}
+	double normalizationFactor = (max - limit)/(max * 1.0);
+	rewind(input);
+	while(!feof(input)){
+		fgets(singleLine, 100, input);
+		if(!isalpha(singleLine[0])){
+			double x = atoi(singleLine);
+			int normalized_output = floor((x-dcShift) * normalizationFactor);
+			if(abs(normalized_output) > 1)
+				fprintf(output, "%d\n", normalized_output);
+		}
+	}
+
+	printf("SAMPLES : %d\nMAX AMPLITUDE : %d\nNORMALIZATION FACTOR : %lf\nDC SHIFT : %lf\n", sampleCount, max, normalizationFactor, dcShift);
 }
